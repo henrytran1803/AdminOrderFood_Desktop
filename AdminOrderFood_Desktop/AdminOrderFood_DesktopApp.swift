effe64358f7ebb1a73a1bd92592d03173d977023
Trần Việt Anh <tranvietanh1803@gmail.com>
2024-06-10 07:46:10 +0700

    Initial Commit

---
+++ AdminOrderFood_Desktop/AdminOrderFood_DesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdminOrderFood_DesktopApp.swift
+//  AdminOrderFood_Desktop
+//
+//  Created by Tran Viet Anh on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AdminOrderFood_DesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdminOrderFood_Desktop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdminOrderFood_Desktop
+//
+//  Created by Tran Viet Anh on 10/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
